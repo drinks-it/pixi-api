39a9ba6d34d628040ba1ac48a8307c08652c293c
smaraz <devnull@localhost>
2016-06-16 13:53:46 +0200

    New location for app soap endpoint is added.

--- config.distro.php
+++ config.distro.php
@@ -1,6 +1,6 @@
 <?php
 
-$uri = "https://leonidas.api.madgeniuses.net/pixiAPP/";
-$location = "https://leonidas.api.madgeniuses.net/pixiAPP/";
+$uri = "https://api.pixi.eu/soap/pixiAPP/";
+$location = "https://api.pixi.eu/soap/pixiAPP/";
 $username = "pixiAPP";
 $password = "fHNzq44NA6kaDm_APP";
