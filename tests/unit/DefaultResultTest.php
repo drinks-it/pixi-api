0d40c1830d40dc23af4440e3ecf6020b57209f3a
Marko Antolovic <ma@pixi.eu>
2016-07-07 11:13:14 +0200

    Added hg ignore and test xml soap response - changed to codeception 2.2

---
+++ tests/unit/DefaultResultTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mantolovic
+ * Date: 6.7.2016
+ * Time: 16:48
+ */
+
+namespace Pixi\API\Soap\Tests;
+
+
+class DefaultResultTest extends \PHPUnit_Framework_TestCase
+{
+
+
+}
